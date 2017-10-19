d3ce30b5728fea45dc19cbc7643e62ea58dcfee6
khanin_daniil <khanin_daniil@mail.ru>
2017-10-19 20:30:56 +0300

    refactoring eslint config

--- generators/webpack-react-template/webpack.config.js
+++ generators/webpack-react-template/webpack.config.js
@@ -58,7 +58,7 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: 'file-loader'
           }
         ]
       }
