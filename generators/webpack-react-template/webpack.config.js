4614c34acd7cf27fcd597a4323f1b58d7ec949ab
khanin_daniil <khanin_daniil@mail.ru>
2017-10-20 15:09:24 +0300

    split loaders

--- generators/webpack-react-template/webpack.config.js
+++ generators/webpack-react-template/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const loaders = require('./webpack_cfg/loaders');
 const env = process.env.NODE_ENV;
 // the path(s) that should be cleaned
 const pathsToClean = [
@@ -22,74 +23,7 @@ module.exports = {
     filename: 'js/[name].bundle.js',
     path: path.resolve(__dirname, './dist')
   },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        use: ['babel-loader'],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          {
-            loader: 'file-loader'
-          }
-        ]
-      },
-      {
-        test: /\.(scss|sass)$/,
-        use: [{
-          loader: 'style-loader'
-        }, {
-          loader: 'css-loader',
-          options: {
-            modules: true
-          }
-        }, {
-          loader: 'sass-loader'
-        }]
-      },
-      {
-        test: /\.less$/,
-        use: [{
-          loader: "style-loader"
-        }, {
-          loader: "css-loader",
-          options: {
-            modules: true
-          }
-        }, {
-          loader: "less-loader"
-        }]
-      }
-      ]
-  },
+  module: loaders(),
   plugins: [
     new CleanWebpackPlugin(pathsToClean, cleanOptions),
     new HtmlWebpackPlugin({
---
+++ generators/webpack-react-template/webpack_cfg/loaders.js
@@ -0,0 +1,44 @@
+module.exports = () => {
+  return {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
