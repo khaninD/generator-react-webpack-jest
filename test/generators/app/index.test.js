fd8c4368a30add390aba9f1655eaccd70c6e6f4a
khanin_daniil <khanin_daniil@mail.ru>
2017-10-23 17:26:12 +0300

    splt test for cssnext and postcss

--- test/generators/app/index.test.js
+++ test/generators/app/index.test.js
@@ -234,6 +234,12 @@ describe('react-webpack-jest:app with support cssmodules (css language)', () => 
     it('should be support react-css-modules', () => {
       assert.fileContent('src/index.js', 'Table = CSSModules(Table, styles);')
     })
+  });
+
+  describe('#content of loaders.js', () => {
+    it('should be support css modules', () => {
+      assert.fileContent('webpack_cfg/loaders.js', 'modules: true')
+    })
   })
 });
 
@@ -263,116 +269,51 @@ describe('react-webpack-jest:app with support cssmodules (sass language)', () =>
     it('should be support sass', () => {
       assert.fileContent('src/index.js', 'import styles from \'./main.sass\';')
     })
+  });
+
+  describe('#content of loaders.js', () => {
+    it('should be support css modules', () => {
+      assert.fileContent('webpack_cfg/loaders.js', 'modules: true')
+    })
   })
 });
-/*
-describe('react-webpack:app', () => {
-  beforeEach(() => beforeLoad());
 
-  describe('test config with customPrompts', () => {
-    beforeEach(() => beforeLoad({
-      appName: 'my-react-app',
-      inlineStyleTool: false
-    }));
+describe('#react-webpack-jest:app support postcss', () => {
+  beforeEach(() => beforeLoad({
+    postcss: true
+  }));
 
-    it ('should be app name === my-react-app', () => {
-      expect(generator.appName).toBe('my-react-app');
-    });
-
-    it('should be not use inlineStyleTools', () => {
-      expect(generator.inlineStyleTool).toBeFalsy();
-      expect(generator.inlineStyleTools).toBeUndefined();
-    });
-  });
-
-  describe('test config with customPrompts **IINLINE STYLE**', () => {
-    beforeEach(() => beforeLoad({
-      inlineStyleTool: true,
-      inlineStyleTools: 'radium'
-    }));
-
-    it('should be use inlineStyleTool', () => {
-      expect(generator.inlineStyleTool).toBeTruthy();
-    });
-
-    it('should be use inlineStyleTools', () => {
-      expect(generator.inlineStyleTools).toBe('radium');
-    });
-
-    it('should add react style support', () => {
-      assert.fileContent('package.json', 'radium');
-    });
-  });
-
-  describe('test config with customPrompts ** STYLES ** ', () => {
-    beforeEach(() => beforeLoad({
-      style: 'sass',
-      cssmodules: true
-    }));
-
-    it('should add reactcss modules support', () => {
-      assert.fileContent('package.json', 'react-css-modules');
-    });
-
-    it('should be use sass', () => {
-      expect(generator.style).toBe('sass');
-    });
-
-    it('cretae file main.sass', () => {
-      assert.file('src/styles/main.sass');
-    });
-
-    it('should add sass-loader  support', () => {
-      assert.fileContent('package.json', 'sass-loader');
-    });
-
-    it('should add sass-loader  support', () => {
-      assert.fileContent('src/index.js', 'import styles from \'./main.sass\';');
-    });
-
-    describe('test config with SCSS styles', () => {
-      beforeEach(() => beforeLoad({
-        style: 'scss',
-        cssmodules: true
-      }));
-      it('should be use scss', () => {
-        expect(generator.style).toBe('scss');
-      });
-
-      it('cretae file main.scss', () => {
-        assert.file('src/styles/main.scss');
-      });
-
-      it('should add sass-loader  support', () => {
-        assert.fileContent('package.json', 'sass-loader');
-      });
-
-      it('should add scss-loader  support', () => {
-        assert.fileContent('src/index.js', 'import styles from \'./main.scss\';');
-      });
-    })
-
-    describe('test config with LESS styles', () => {
-      beforeEach(() => beforeLoad({
-        style: 'less',
-        cssmodules: true
-      }));
-      it('should be use less', () => {
-        expect(generator.style).toBe('less');
-      });
-
-      it('create main.less file', () => {
-        assert.file('src/styles/main.less');
-      });
-
-      it('should add sass-loader  support', () => {
-        assert.fileContent('package.json', 'less-loader');
-      });
-
-      it('should add scss-loader  support', () => {
-        assert.fileContent('src/index.js', 'import styles from \'./main.less\';');
-      });
+  describe('#config', () => {
+    it('should be use postcss', () => {
+      expect(generator.postcss).toBeTruthy();
     })
   });
+
+  describe('#configuring', () => {
+    it('should be support postcss', () => {
+      assert.fileContent('package.json', 'postcss-loader')
+    })
+  })
 });
-*/
+
+describe('#react-webpack-jest:app support nextcss', () => {
+  beforeEach(() => beforeLoad({
+    cssnext: true
+  }));
+
+  describe('#config', () => {
+    it('should be use cssnext', () => {
+      expect(generator.cssnext).toBeTruthy();
+    })
+  });
+
+  describe('#configuring', () => {
+    it('should be support postcss', () => {
+      assert.fileContent('package.json', 'postcss-loader')
+    });
+
+    it('should be support cssnext', () => {
+      assert.fileContent('package.json', 'postcss-cssnext')
+    })
+  })
+});
